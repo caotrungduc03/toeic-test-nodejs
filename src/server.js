24f0c9cbc5cc0751ba823746ebecf1504be4731a
caotrungduc <trungdc2003@gmail.com>
2023-06-21 18:42:02 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routers');
+const errorMiddleware = require('./middlewares/error.middleware');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+const mongoURI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/toeic-test';
+
+(async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log('Connect database successfully!');
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+app.use(express.json());
+app.use(router);
+
+app.use(errorMiddleware);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
